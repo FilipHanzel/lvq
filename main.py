8da9752890d5ca10d65d52cbe5ded27e47436668
Filip <filip.han@interia.pl>
2022-04-23 18:52:52 +0200

    improved model testing

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
 import csv
-from pprint import pprint
+from random import choice
 
 import lvq
 from data import normalize
+
 
 if __name__ == "__main__":
     with open("iris-data.csv", "rt") as f:
         dataset = [[float(value) for value in line] for line in csv.reader(f) if line]
     dataset = normalize(dataset)
 
-    features = 4
-    labels = 3
-    codebook = 10
-
-    model = lvq.LVQ(
-        codebook_size=codebook,
-        features_count=features,
-        labels_count=labels,
+    model_config = dict(
+        codebook_size=6,
+        features_count=4,
+        labels_count=3,
         codebook_init_method="sample",
         codebook_init_dataset=dataset,
     )
 
-    print("Sample:", dataset[0])
-    print("Prediction:", model.predict(dataset[0]))
+    model = lvq.LVQ(**model_config)
 
-    print("New codebook:")
-    pprint(model.codebook)
+    sample = choice(dataset)
+    print("Random sample:", [round(value, 3) for value in sample])
+    *features, label = sample
 
+    print("Prediction:", model.predict(features))
+    print("Initialized codebook:")
+    for vector in model.codebook:
+        print("\t", [round(value, 3) for value in vector])
+
+    print("Training model...")
     model.train_codebook(train_vectors=dataset, base_learning_rate=0.001, epochs=100)
 
+    print("Prediction:", model.predict(features))
     print("Trained codebook:")
-    pprint(model.codebook)
+    for vector in model.codebook:
+        print("\t", [round(value, 3) for value in vector])
 
-    print("Sample:", dataset[0])
-    print("Prediction:", model.predict(dataset[0]))
-
+    print("Cross validating model...")
     scores = lvq.cross_validate(
         dataset,
         5,
-        codebook_size=codebook,
-        features_count=features,
-        labels_count=labels,
-        codebook_init_method="sample",
-        codebook_init_dataset=dataset,
+        **model_config,
         learning_rate=0.01,
         epochs=100,
     )
-    print("Cross validation scores:")
-    pprint(scores)
+    print("Cross validation scores:", [round(score, 3) for score in scores])
+    print("Average:", round(sum(scores) / len(scores), 3))
