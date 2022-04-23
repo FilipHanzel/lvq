9eb203cd0fbcb56594d8c9b637821bcf0c3e59c4
Filip <filip.han@interia.pl>
2022-04-23 20:36:30 +0200

    added ionosphere dataset, label encoding  and improved data loading

--- data.py
+++ data.py
@@ -1,5 +1,41 @@
-# Add normalization here
-from typing import List, Dict
+import os
+import csv
+from typing import List, Dict, Any
+
+
+def load_iris_data():
+    with open(os.path.join("data", "iris-data.csv"), "rt") as f:
+        dataset = [
+            [float(value) for value in features] + [label]
+            for *features, label in [line for line in csv.reader(f) if line]
+        ]
+    mapping, encoded = encode_labels(dataset)
+
+    normalized = normalize(encoded)
+    return mapping, normalized
+
+
+def load_ionosphere_data():
+    with open(os.path.join("data", "ionosphere-data.csv"), "rt") as f:
+        dataset = [
+            [float(value) for value in features] + [label]
+            for *features, label in [line for line in csv.reader(f) if line]
+        ]
+
+    mapping, encoded = encode_labels(dataset)
+
+    return mapping, encoded
+
+
+def encode_labels(dataset: List[List[Any]]) -> (Dict[Any, int], List[List[Any]]):
+    """Encode dataset labels. Return label: encoding mapping and mapped dataset."""
+
+    mapping = {
+        label: idx + 1 for idx, label in enumerate({label for *_, label in dataset})
+    }
+    mapped_dataset = [[*features, mapping[label]] for *features, label in dataset]
+
+    return mapping, mapped_dataset
 
 
 def normalize(dataset: List[List[float]]) -> List[List[float]]:
--- main.py
+++ main.py
@@ -1,35 +1,52 @@
-import csv
 from random import choice, seed
+from pprint import pprint
+import argparse
 
 import lvq
-from data import normalize
+from data import normalize, encode_labels
+from data import load_ionosphere_data, load_iris_data
+
+
+def pprint_vector(vector):
+    vector = [str(round(value, 3)) for value in vector]
+    if len(vector) > 6:
+        vector = [*vector[:3], "...", *vector[-3:]]
+    print("[" + ", ".join([f"{value:>7}" for value in vector]) + "]")
 
 
 if __name__ == "__main__":
-    with open("iris-data.csv", "rt") as f:
-        dataset = [[float(value) for value in line] for line in csv.reader(f) if line]
-    dataset = normalize(dataset)
+    labels_mapping, dataset = load_ionosphere_data()
+    # labels_mapping, dataset = load_iris_data()
+    
+    print("Label mapping:")
+    for label in labels_mapping:
+        print(f"\t{label + ':':20} {labels_mapping[label]}")
 
     seed(0)
 
+    sample = choice(dataset)
+    *features, label = sample
+
+    features_count = len(features)
+    labels_count = len(labels_mapping)
+
     model_config = dict(
         codebook_size=6,
-        features_count=4,
-        labels_count=3,
+        features_count=features_count,
+        labels_count=labels_count,
         codebook_init_method="sample",
         codebook_init_dataset=dataset,
     )
 
     model = lvq.LVQ(**model_config)
 
-    sample = choice(dataset)
-    print("Random sample:", [round(value, 3) for value in sample])
-    *features, label = sample
+    print("Random sample:")
+    pprint_vector(sample)
 
     print("Prediction:", model.predict(features))
     print("Initialized codebook:")
     for vector in model.codebook:
-        print("\t", [round(value, 3) for value in vector])
+        pprint_vector(vector)
 
     print("Training model...")
     model.train_codebook(train_vectors=dataset, base_learning_rate=0.001, epochs=100)
@@ -37,7 +54,7 @@ if __name__ == "__main__":
     print("Prediction:", model.predict(features))
     print("Trained codebook:")
     for vector in model.codebook:
-        print("\t", [round(value, 3) for value in vector])
+        pprint_vector(vector)
 
     print("Cross validating model...")
     scores = lvq.cross_validate(
